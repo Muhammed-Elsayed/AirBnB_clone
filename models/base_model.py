9250e6488b90aace2a85ff441b272e7e5885b0f2
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-08-13 18:53:40 +0300

    ..

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 from datetime import datetime
-import uuid
-import models
 from uuid import uuid4
 
 
@@ -10,28 +8,22 @@ from uuid import uuid4
 
 class BaseModel:
     """BaseModel Class"""
-
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel.
+        """used to initialize the object and to covert dict to its obj"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self,
+                            key,
+                            datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
 
-        Args:
-            *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes.
-        """
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
-                else:
-                    self.__dict__[k] = v
+                elif key != "__class__":
+                    setattr(self, key, value)
+
         else:
-            models.storage.new(self)
-
-
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     """represinting the object in the form of str"""
     def __str__(self):
